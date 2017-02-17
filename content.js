0def5b98662f217a79a664197300e54373b918d1
Brian Hurst <hursey013@gmail.com>
2017-02-17 10:44:55 -0500

    Use template literals

--- content.js
+++ content.js
@@ -1,19 +1,17 @@
 var withdrawals = document.querySelectorAll('.dashboard .bg-success');
+var checking = document.querySelector('#balances > div:nth-child(2) .media-right h4');
+var debitize = document.querySelector('#balances > div:nth-child(3) .media-right h4');
+var credit = document.querySelector('#balances > div:nth-child(4) .media-right h4');
 
 // Adjust width of dashboard columns
 for (let i = 0; i < withdrawals.length; ++i) {
   withdrawals[i].parentNode.parentNode.className = 'col-md-4';
 }
 
-// Get current balances of various accounts
-var checking = document.querySelector('#balances > div:nth-child(2) .media-right h4');
-var debitize = document.querySelector('#balances > div:nth-child(3) .media-right h4');
-var credit = document.querySelector('#balances > div:nth-child(4) .media-right h4');
-
 // Subtract the balance of checking & debitize account from credit balance
 var spend = (getNumber(checking) + getNumber(debitize)) - getNumber(credit);
 
-// Currency formatting function
+// Currency formatting
 var formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
@@ -22,14 +20,15 @@ var formatter = new Intl.NumberFormat('en-US', {
 
 // Insert new spend column into dashboard
 document.querySelector('.dashboard > .row > div:nth-child(2)').insertAdjacentHTML('afterend',
-  '<div class="col-md-4"><div class="panel panel-default">' +
-  '<div class="panel-heading"><h3 class="panel-title">Safe to Spend</h3></div>' +
-  '<div class="panel-body text-nowrap bg-' + (spend > 0 ? "success" : "danger") + '">' +
-  formatter.format(spend) +
-  '</div>' +
-  '</div></div>');
+  `<div class="col-md-4"><div class="panel panel-default">
+    <div class="panel-heading"><h3 class="panel-title">Safe to Spend</h3></div>
+      <div class="panel-body text-nowrap bg-${(spend > 0) ? 'success' : 'danger'}">
+        ${formatter.format(spend)}
+      </div>
+    </div>
+  </div>`);
 
 // Strip symbols from numbers
 function getNumber(val) {
-  return Number(val.innerHTML.replace(/[^0-9\.]+/g,""));
+  return Number(val.innerHTML.replace(/[^0-9\.]+/g,''));
 }
